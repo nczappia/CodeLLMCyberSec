498ea5176748f392994f6dfabd2a90bc572d9189
nczappia <nczappia15@gmail.com>
2024-07-17 08:28:43 -0500

    Finished Cleaning Datasets

--- DeepSeek-Coder/finetune.py
+++ DeepSeek-Coder/finetune.py
@@ -1,113 +1,189 @@
+import argparse
+import multiprocessing
+import os
+
+import torch
 import transformers
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-from torch.utils.data import DataLoader
-from tqdm.auto import tqdm  # Progress bar (optional)
+from accelerate import PartialState
+from datasets import load_dataset
+from peft import LoraConfig
+from transformers import (
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+    logging,
+    set_seed,
+)
+from trl import SFTTrainer
 
-learning_rate = 2e-5
-batch_size = 8
-num_train_epochs = 3
-
-model_name = "deepseek-ai/deepseek-coder-6.7b-instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
-
-#***Prepare the dataset***
-import torch
-from transformers import PreTrainedTokenizer
-import pyarrow.parquet as pq
-
-class CodeGenDataset(torch.utils.data.Dataset):
-    def __init__(self, data_path, tokenizer: PreTrainedTokenizer):
-        self.data = self._load_data(data_path)
-        self.tokenizer = tokenizer
-
-    def _load_data(self, data_path):
-        # Load data from Parquet using pyarrow
-        data = pq.read_table(data_path)
-
-        return data
+# Command: accelerate launch finetune.py         --model_id "deepseek-ai/deepseek-coder-6.7b-instruct"         --dataset_name "iamtarun/code_instructions_120k_alpaca"         --subset "data/train-00000-of-00001-d9b93805488c263e.parquet"         --dataset_text_field "output"         --split "train"         --max_seq_length 512         --max_steps 10000         --micro_batch_size 2         --gradient_accumulation_steps 8         --learning_rate 2e-5         --warmup_steps 20         --num_proc "$(nproc)"
 
 
-    def __len__(self):
-        return len(self.data)
 
-    def __getitem__(self, idx):
-        # Get prompt and code text from data
-        prompt = self.data.take([idx]).column('instruction').to_numpy()[0]
-        code = self.data.take([idx]).column('output').to_numpy()[0]
 
-        # Tokenize and pad prompt and code directly using tokenizer
-        padded_prompt = self.tokenizer(
-            prompt, return_tensors="pt", padding="max_length", add_special_tokens=True
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_id", type=str, default="deepseek-ai/deepseek-coder-6.7b-instruct")
+    parser.add_argument("--dataset_name", type=str, default="code_instructions_120k_alpaca")
+    parser.add_argument("--subset", type=str, default="data/train-00000-of-00001-d9b93805488c263e.parquet")
+    parser.add_argument("--split", type=str, default="train")
+    parser.add_argument("--dataset_text_field", type=str, default="content")
+
+    parser.add_argument("--max_seq_length", type=int, default=1024)
+    parser.add_argument("--max_steps", type=int, default=1000)
+    parser.add_argument("--micro_batch_size", type=int, default=1)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=4)
+    parser.add_argument("--weight_decay", type=float, default=0.01)
+    parser.add_argument("--bf16", type=bool, default=True)
+
+    parser.add_argument("--attention_dropout", type=float, default=0.1)
+    parser.add_argument("--learning_rate", type=float, default=2e-4)
+    parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
+    parser.add_argument("--warmup_steps", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--output_dir", type=str, default="finetune_starcoder2")
+    parser.add_argument("--num_proc", type=int, default=None)
+    parser.add_argument("--push_to_hub", type=bool, default=True)
+    parser.add_argument("--main_process_port", type=int, default=0)
+    return parser.parse_args()
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+def main(args):
+    # config
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+    lora_config = LoraConfig(
+        r=8,
+        target_modules=[
+            "q_proj",
+            "o_proj",
+            "k_proj",
+            "v_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+        task_type="CAUSAL_LM",
+    )
+
+    # load model and dataset
+    token = os.environ.get("HF_TOKEN", None)
+    # print out selected device
+    print(PartialState().process_index)
+
+    #Check for GPU availability
+    print(torch.cuda.is_available())
+    if torch.cuda.is_available():
+        print("GPU")
+        device = torch.device("cuda:0")
+
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model_id,
+            quantization_config=bnb_config,
+            attention_dropout=args.attention_dropout,
         )
-        padded_code = self.tokenizer(
-            code, return_tensors="pt", padding="max_length", add_special_tokens=True
+    else:
+        #Handle no GPU availiability
+        print("No GPU")
+        device = torch.device("cpu")
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model_id,
+            quantization_config=bnb_config,
+            attention_dropout=args.attention_dropout,
         )
-
-        # Extract data from padded outputs
-        input_ids = padded_prompt['input_ids']
-        attention_mask = padded_prompt['attention_mask']  # Attention mask for padding
-        labels = padded_code['input_ids']  # Use input_ids here for labels (shifted by 1 for causality)
-
-        return {
-            'input_ids': input_ids,
-            'attention_mask': attention_mask,
-            'labels': labels
-        }
-
-        
-
-
-    def _pad(self, sequence, padding_value=0):
-        # This example pads sequences to the maximum length in the batch
-        max_len = max(len(seq) for seq in self.data)
-        print("Length before padding:", sequence.shape)
-        padded_sequence = torch.nn.functional.pad(sequence, (0, max_len - len(sequence)), value=padding_value)
-        print("max_len:", max_len, "padded sequence len:", padded_sequence.shape)
-        print("padded_sequence", padded_sequence, '\n')
-        return torch.nn.functional.pad(sequence, (0, max_len - sequence.shape[1]), value=padding_value)
     
-code_120k = CodeGenDataset("../train-00000-of-00001-d9b93805488c263e.parquet", tokenizer)
+    #if torch.cuda.device_count() > 1:
+    #    print("Using DataParallel for multiple GPUs")
+    #    model = torch.nn.DataParallel(model)
 
-print(len(code_120k))
+    
+    #model.to(device)
+
+    print_trainable_parameters(model)
+
+    print("Model loaded")
+
+    print("Starting Dataset")
+
+    print(args.dataset_name)
+    print(args.subset)
+    print(args.split)
+    print(token)
+    print(args.num_proc if args.num_proc else multiprocessing.cpu_count())
+    
+    data = load_dataset(
+        args.dataset_name,
+        data_files=args.subset,
+        split=args.split,
+        token=token,
+        num_proc=args.num_proc if args.num_proc else multiprocessing.cpu_count(),
+    )
+
+    data = data.remove_columns(['input', 'prompt'])
+
+    data = data.filter(lambda example: example['output'] != '')
+
+    print('Dataset type: ', type(data))
+
+    # setup the trainer
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=data,
+        max_seq_length=args.max_seq_length,
+        args=transformers.TrainingArguments(
+            per_device_train_batch_size=args.micro_batch_size,
+            gradient_accumulation_steps=args.gradient_accumulation_steps,
+            warmup_steps=args.warmup_steps,
+            max_steps=args.max_steps,
+            learning_rate=args.learning_rate,
+            lr_scheduler_type=args.lr_scheduler_type,
+            weight_decay=args.weight_decay,
+            bf16=args.bf16,
+            logging_strategy="steps",
+            logging_steps=10,
+            output_dir=args.output_dir,
+            optim="paged_adamw_8bit",
+            seed=args.seed,
+            run_name=f"train-{args.model_id.split('/')[-1]}",
+            report_to="wandb",
+        ),
+        peft_config=lora_config,
+        dataset_text_field=args.dataset_text_field,
+    )
+
+    # launch
+    print("Training...")
+    trainer.train()
+
+    print("Saving the last checkpoint of the model")
+    model.save_pretrained(os.path.join(args.output_dir, "final_checkpoint/"))
+    if args.push_to_hub:
+        trainer.push_to_hub("Upload model")
+    print("Training Done! 💥")
 
 
-# Replace "your_dataset" with your actual dataset object
-train_dataloader = DataLoader(code_120k, batch_size=batch_size, shuffle=True)
+if __name__ == "__main__":
+    args = get_args()
+    set_seed(args.seed)
+    os.makedirs(args.output_dir, exist_ok=True)
 
+    logging.set_verbosity_error()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-criterion = torch.nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)  # Adjust for masking
-
-model.train()  # Set model to training mode
-
-for epoch in range(num_train_epochs):
-    for data in tqdm(train_dataloader):
-        input_ids = data['input_ids'].to(model.device)
-        attention_mask = data['attention_mask'].to(model.device)
-        print('Attention Mask:', attention_mask.shape)
-        labels = data['labels'].to(model.device)
-        print(attention_mask)
-
-        attention_mask = attention_mask.unsqueeze(1)  # Add a new dimension of size 1 at index 1
-
-        attention_mask = 1 - attention_mask
-
-        # Forward pass
-        outputs = model(input_ids, attention_mask=attention_mask)
-        logits = outputs.last_hidden_state
-
-        # Loss calculation
-        loss = criterion(logits.view(-1, tokenizer.vocab_size), labels.view(-1))
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # (Optional) Track and log training metrics (loss, accuracy)
-
-# After training, save the fine-tuned model for future use
-#model.save_pretrained("my_fine-tuned_deepseek_coder")
-#tokenizer.save_pretrained("my_fine-tuned_deepseek_coder")
+    main(args)
